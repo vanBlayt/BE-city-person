4bfbbe65fefa7df2d393fb669c3d18bf8f4080b1
van <1075236902@qq.com>
2022-03-26 00:07:19 +0800

    feat: 修改登录状态码

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards, HttpCode } from '@nestjs/common';
 import { User } from '../schemas/user.schema';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { TokenEntity } from './token.entity';
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
   @UseGuards(AuthGuard('local'))
   @Post('/login')
+  @HttpCode(200)
   async login(@Request() request): Promise<TokenEntity> {
     return this.authService.login(request.user);
   }
